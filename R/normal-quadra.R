b6943895d9151da761af169af831c97c8e59cdf5
liuhuan90123 <liuhuanbnu@gmail.com>
2021-09-19 19:01:54 -0500

    two functions

---
+++ R/normal-quadra.R
@@ -0,0 +1,27 @@
+#' Generate Gaussian quadrature points and corresponding weights
+#'
+#' @param nnodes Number of quadrature points
+#' @param minmax Absolute value of minumum or maximum of quadrature points
+#'
+#' @return A list of Gaussian quadrature points and corresponding weights
+#' @export
+#'
+#' @examples
+#' normal_quadra(41, 5)
+#'
+#'
+normal_quadra <- function(nnodes, minmax){
+
+  # set nodes ranging from -5 to 5
+  nodes <- seq(-minmax, minmax, length.out = nnodes)
+
+  # unnormalized weights
+  weightsUnwtd <- sapply(nodes, FUN = function(x) dnorm(x))
+
+  # normalized weightes
+  weightsWtd <- weightsUnwtd / sum(weightsUnwtd)
+
+  # return nodes and normalized weights
+  return(list("nodes" = nodes, "weights" = weightsWtd))
+
+}
---
+++ dev/pd.R
@@ -0,0 +1,47 @@
+library(devtools)
+library(fs)
+library(roxygen2)
+library(rmarkdown)
+library(usethis)
+# library(renv)
+
+
+# install.packages("usethis")
+
+# install.packages("usethis")
+
+## create package
+# create_package("C:\\Users\\liuhu\\OneDrive - University of Iowa\\Github\\emreliability")
+
+
+
+# use_git()
+#
+# check()
+#
+# %>%
+#
+# load_all()
+
+# install.packages("renv")
+# renv::init()
+# renv::snapshot()
+
+# 如果有引入新的包，运行 renv::snapshot() 进行 lockfile 的更新；
+# 如果想更新所使用的包，运行 renv::update() 进行包本身的更新；
+# 如果换了电脑进行开发，运行 renv::restore() 恢复到开发时用的包环境
+
+# install.packages("rmarkdown")
+
+
+# usethis::use_readme_rmd()
+
+
+
+# devtools::document()
+
+
+
+
+
+
